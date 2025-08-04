b0312a56d5572f8a37655c4a0e5b4f88547761d6
effilin <effienummer@gmail.com>
2025-08-04 14:24:19 -0400

    new c# learning app

---
+++ WebApplication1/WebApplication1/Program.cs
@@ -0,0 +1,20 @@
+//var builder = WebApplication.CreateBuilder(args);
+//var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+
+//app.Run();
+
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello again");
+    }
+  }
+}
